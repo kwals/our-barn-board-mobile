4abc7666b3c6c28a87a4dd06ae1a6ab89002a3ab
Katie Walsh <katie.katiewalsh@gmail.com>
2015-06-18 14:33:06 -0400

    Set nav controller

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,13 +3,11 @@ class AppDelegate
      @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
       @window.makeKeyAndVisible
       @defaults = NSUserDefaults.standardUserDefaults
-      @defaults["email"] = "laura@example.com"
-      @defaults["token"] = "HWKgyXWGKAgdLYByGdL8"
-      # @task_controller = UINavigationController.alloc.initWithRootViewController(TaskController.alloc.initWithStyle(UITableViewStylePlain))
+      @task_controller = UINavigationController.alloc.initWithRootViewController(TaskController.alloc.initWithStyle(UITableViewStylePlain))
       @window.rootViewController = @task_controller
       @login_controller = LoginController.alloc.initWithNibName(nil, bundle:nil)
       @navigation_controller = UINavigationController.alloc.initWithRootViewController(@login_controller)
-      @window.rootViewController = @navigation_controller
+      # @window.rootViewController = @navigation_controller
       true
   end
 end
